73e8794203351c575c94050a6a46d716a71f61ab
Pradnya Desai <115803674+PRADNYA-DESAI-15@users.noreply.github.com>
2024-01-23 10:47:38 +0530

    bootcamp

---
+++ forloop.sh
@@ -0,0 +1,4 @@
+for((i=0; i<=6 ;i++))
+do
+echo "hey , this is shallscripting peogram"
+done
---
+++ if.sh
@@ -0,0 +1,7 @@
+read -p "enter the no. :" a
+read -p "enter the no. :" b
+if [$a -gt $b ]
+then
+echo $a is greater than $b
+fi
+
